6f035a3a1d19510b69de7c95ab436e2705ce452b
FabiaCode <f.baumgartner@student.htl-leonding.ac.at>
2025-12-15 15:03:28 +0100

    hamburger in systems.css angepasst, wie in benefits.css

--- js/systems.js
+++ js/systems.js
@@ -1,24 +1,8 @@
+// Mobile menu toggle
 const hamburger = document.getElementById("hamburger");
-const modalOverlay = document.getElementById("modalOverlay");
-const modalBox = document.querySelector(".modal-box");
-const closeBtn = document.querySelector(".close-btn");
+const mobileMenu = document.getElementById("mobileMenu");
 
-// Öffnen
 hamburger.addEventListener("click", () => {
-  modalOverlay.style.display = "flex";
-});
-
-// Schließen über X
-closeBtn.addEventListener("click", () => {
-  modalOverlay.style.display = "none";
-});
-
-// Klick im Menü darf NICHT schließen
-modalBox.addEventListener("click", (e) => {
-  e.stopPropagation();
-});
-
-// Schließen nur bei Klick auf dunklen Hintergrund
-modalOverlay.addEventListener("click", () => {
-  modalOverlay.style.display = "none";
+    mobileMenu.classList.toggle("open");
+    mobileMenu.style.display = mobileMenu.classList.contains("open") ? "block" : "none";
 });

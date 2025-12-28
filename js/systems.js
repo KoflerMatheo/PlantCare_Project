7ce1652c00529e86b2f075a7940026eda741eb95
FabiaCode <f.baumgartner@student.htl-leonding.ac.at>
2025-12-28 17:14:44 +0100

    in systems.js die versteckten Vor- und Nachteile auf- und zu machen

--- js/systems.js
+++ js/systems.js
@@ -6,3 +6,15 @@ hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("open");
     mobileMenu.style.display = mobileMenu.classList.contains("open") ? "block" : "none";
 });
+
+// Akkordeon Vorteile / Nachteile
+document.querySelectorAll(".toggle-btn").forEach(button => {
+  button.addEventListener("click", () => {
+    const box = button.nextElementSibling;
+    box.classList.toggle("hidden");
+
+    button.textContent = box.classList.contains("hidden")
+      ? "Anzeigen"
+      : "Ausblenden";
+  });
+});
